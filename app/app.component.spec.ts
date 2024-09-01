4f3227e1b0d6e699ebb5a8ad12fb981a7ad31e28
Jeniffer <jheni.sribeiro2020@gmail.com>
2024-09-01 20:56:18 -0300

    first commit

---
+++ app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent],
+    imports: [IonicModule.forRoot()]
+  }).compileComponents());
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  // Adicione mais testes conforme necessário
+});
